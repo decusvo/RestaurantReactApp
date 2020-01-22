de4d71add8a7e45847b2ae514ba3490975820af3
Jack <jack.pp@btopenworld.com>
2020-01-22 17:57:52 +0000

    Added first version of schema and function for state transition. TODO add mechanism to automatically update states when events are added to database, see example: https://felixge.de/2017/07/27/implementing-state-machines-in-postgresql.html

---
+++ func_schema.sql
@@ -0,0 +1,60 @@
+/*
+	Order States:
+		- start
+		- requested
+		- confirmed
+		- cooking
+		- delivered
+		- paid
+		- cancelled
+
+	Order Events:
+		- request
+		- confirm
+		- start_cook
+		- deliver
+		- pay
+		- cancel
+*/
+
+DROP FUNCTION IF EXISTS order_event_transition;
+
+CREATE FUNCTION order_event_transition(state text, event text) RETURNS text 
+LANGUAGE sql AS
+$$
+	SELECT CASE state	
+		WHEN 'start' THEN
+			CASE event
+				WHEN 'request' THEN 'requested'
+				ELSE 'error'
+			END
+
+		WHEN 'requested' THEN
+			CASE event
+				WHEN 'confirm' THEN 'confirmed'
+				ELSE 'error'
+			END
+	
+		WHEN 'confirmed' THEN
+			CASE event
+				WHEN 'cancel' THEN 'cancelled'
+				WHEN 'start_cook' THEN 'cooking'
+				ELSE 'error'
+			END
+
+		WHEN 'cooking' THEN
+			CASE event
+				WHEN 'cancel' THEN 'cancelled'
+				WHEN 'deliver' THEN 'delivered'
+				ELSE 'error'
+			END
+
+		WHEN 'delivered' THEN
+			CASE event
+				WHEN 'cancel' THEN 'cancelled'
+				WHEN 'pay' THEN 'paid'
+				ELSE 'error'
+			END
+	END
+$$;
+
