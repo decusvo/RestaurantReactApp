9f5405e769cd1c3fd8d210274cae66c00f804196
JamieBliss <zfac223@example.com>
2020-01-26 18:39:06 +0000

    added comments explaining code

--- Tutorials/Jamie/restful_api_in_flask/a_quick_start/resource_routing.py
+++ Tutorials/Jamie/restful_api_in_flask/a_quick_start/resource_routing.py
@@ -4,16 +4,18 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
+todos = {}  #stores the data for the todo set
 
 class TodoSimple(Resource):
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        return {todo_id: todos[todo_id]}  # returns the data about the todo id parsed to it
 
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+        todos[todo_id] = request.form['data']  # requests the data from the user and adds it to the set
+        return {todo_id: todos[todo_id]}  # returns the data inputed
 
+# adds the class TodoSimple to be called at the url '/'
+#  it also takes an input string which is assigned to the variable todo_id
 api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
