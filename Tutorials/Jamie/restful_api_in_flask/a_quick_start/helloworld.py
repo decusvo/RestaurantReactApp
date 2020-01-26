aa8f85281ee65e9d2fe522a1903cc22fb639160e
JamieBliss <zfac223@example.com>
2020-01-26 18:10:25 +0000

    added breif comments describing code

--- Tutorials/Jamie/restful_api_in_flask/a_quick_start/helloworld.py
+++ Tutorials/Jamie/restful_api_in_flask/a_quick_start/helloworld.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-app = Flask(__name__)
+app = Flask(__name__)  # creates an instance of flask
 api = Api(app)
 
-class HelloWorld(Resource):
+class HelloWorld(Resource):  # class returns hello world from a get request
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/')  # assigns the url '/' to call the class HelloWorld
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # runs the server in dubugger mode
