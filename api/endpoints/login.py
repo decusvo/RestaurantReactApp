f6619e33a48209b6492c2bcc41cbc1f3ec83e28f
Jamie Bliss <zfac223@live.rhul.ac.uk>
2020-01-30 11:02:35 +0000

    implemented basic user authentication

--- api/endpoints/login.py
+++ api/endpoints/login.py
@@ -29,9 +29,28 @@ def login():
     if(not result):
         return jsonify(valid_credentials=False)
 
-    return jsonify(result)
+    #  get the password of the user
+    cur.execute("SELECT password, firstname " +
+      "FROM customer " +
+      "WHERE email = '%s'" %email)
+    result = cur.fetchall()
+
+    #  checks if the password returned is the same as the one inputted
+    if result[0][0] != password:
+        return jsonify(valid_credentials=False)
+
+    return jsonify(name = result[0][1], valid_credentials = True)
 
 
 if __name__ == "__main__":
     app.debug = True
     app.run(port=5000)
+
+
+###  Temporary tests to run to test the file
+
+#  curl -d '{"email":"example@example.com", "password":"password"}' -H "Content-type: application/json"  -X POST "127.0.0.1:5000/login"
+#  result should return their name and that the input was valid
+
+#  curl -d '{"email":"notanemail@invalid.com", "password":"incorrect_input"}' -H "Content-type: application/json"  -X POST "127.0.0.1:5000/login"
+#  result should return invalid credentials
