4eaa4ddf1dfcf9635e8c7e80073c5ef1106ffb39
dnzggg <45202160+dnzggg@users.noreply.github.com>
2020-01-30 10:28:24 +0000

    Menu renders the whole menu.

--- src/Components/FoodMenu.js
+++ src/Components/FoodMenu.js
@@ -36,20 +36,72 @@ export default class FoodMenu extends React.Component{
                     </Typography>
                     <Button onClick={this.print}> </Button>
 
+                    <Typography style={{marginTop: 10, fontSize: 20}} color={"textPrimary"} gutterBottom>
+                        Starters
+                    </Typography>
                     {
                         this.state.items.map(function (dishes, index) {
                             const dish = dishes["0"];
-                            // const type = dish.type; for rendering starters only
-                            return (
-                                <FoodMenuItem key={index} value={dish.name} description={dish.description} price={dish.price} calories={dish.calories}/>
-                            );
-                            // if (type === "starter") {
-                            //     return (
-                            //         <FoodMenuItem key={index} value={dish.name} description={dish.description} price={dish.price} calories={dish.calories}/>
-                            //     );
-                            // } else {
-                            //     return(<div key={index}/>);
-                            // } for rendering starters only
+                            const type = dish.type;
+
+                            if (type === "starter") {
+                                return (
+                                    <FoodMenuItem key={index} value={dish.name} description={dish.description} price={dish.price} calories={dish.calories}/>
+                                );
+                            } else {
+                                return (<div key={index}> </div>);
+                            }
+                        })
+                    }
+                    <Typography style={{marginTop: 10, fontSize: 20}} color={"textPrimary"} gutterBottom>
+                        Sides
+                    </Typography>
+                    {
+                        this.state.items.map(function (dishes, index) {
+                            const dish = dishes["0"];
+                            const type = dish.type;
+
+                            if (type === "side") {
+                                return (
+                                    <FoodMenuItem key={index} value={dish.name} description={dish.description} price={dish.price} calories={dish.calories}/>
+                                );
+                            } else {
+                                return (<div key={index}> </div>);
+                            }
+                        })
+                    }
+                    <Typography style={{marginTop: 10, fontSize: 20}} color={"textPrimary"} gutterBottom>
+                        Mains
+                    </Typography>
+                    {
+                        this.state.items.map(function (dishes, index) {
+                            const dish = dishes["0"];
+                            const type = dish.type;
+
+                            if (type === "main") {
+                                return (
+                                    <FoodMenuItem key={index} value={dish.name} description={dish.description} price={dish.price} calories={dish.calories}/>
+                                );
+                            } else {
+                                return (<div key={index}> </div>);
+                            }
+                        })
+                    }
+                    <Typography style={{marginTop: 10, fontSize: 20}} color={"textPrimary"} gutterBottom>
+                        Desserts
+                    </Typography>
+                    {
+                        this.state.items.map(function (dishes, index) {
+                            const dish = dishes["0"];
+                            const type = dish.type;
+
+                            if (type === "dessert") {
+                                return (
+                                    <FoodMenuItem key={index} value={dish.name} description={dish.description} price={dish.price} calories={dish.calories}/>
+                                );
+                            } else {
+                                return (<div key={index}> </div>);
+                            }
                         })
                     }
 
