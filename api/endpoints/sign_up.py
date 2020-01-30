82c65879196aab51e8c81d5107be6aaad3dda11d
Jamie Bliss <zfac223@live.rhul.ac.uk>
2020-01-30 13:03:25 +0000

    implemented the ability for customers to sign up

--- api/endpoints/sign_up.py
+++ api/endpoints/sign_up.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, jsonify
 import psycopg2
 
+###
+###  makes connection to a psql database
+###  this is temporary!!
+###
+connection = psycopg2.connect(user = "jrngriijazlzxr",
+                                  password = "655c5947f8ad0f453bfd4b5d4b80d6374a3a97f706c4533a60be75c3de343817",
+                                  host = "ec2-54-247-72-30.eu-west-1.compute.amazonaws.com",
+                                  port = "5432",
+                                  database = "d9vtdql067f73n")
+cur = connection.cursor()
 
 app = Flask(__name__)
 
@@ -12,8 +22,14 @@ def sign_up():
     firstname = request.json.get('firstname')
     lastname = request.json.get('lastname')
 
-    #  returns the data recived through the post 
-    return jsonify(firstname = firstname, lastname = lastname, email = email, password = password)
+    try:
+        cur.execute("INSERT INTO customer VALUES ('{}','{}','{}','{}')".format(email, firstname, lastname, password))
+    except psycopg2.errors.UniqueViolation:
+        return jsonify(success = False)
+
+    connection.commit()
+
+    return jsonify(success = True)
 
 
 if __name__ == "__main__":
@@ -24,4 +40,4 @@ if __name__ == "__main__":
 #  Temporary test commands
 
 #  curl -d '{"email":"testing@test.com", "password":"password", "firstname":"DELETE", "lastname":"ME"}' -H "Content-type: application/json"  -X POST "127.0.0.1:5000/sign_up"
-#  this should just print all the details inputted
+#  this should return a json saying if it was successful or not
