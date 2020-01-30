a0d1b3350b45c59d430f5e25043ccd4e15f03d74
Jamie Bliss <zfac223@live.rhul.ac.uk>
2020-01-30 10:39:53 +0000

    now uses the email to check if the user exists in the database

--- api/endpoints/login.py
+++ api/endpoints/login.py
@@ -2,6 +2,17 @@ from flask import Flask, request, jsonify
 import psycopg2
 
 
+###
+###  makes connection to a psql database
+###  this is temporary!!
+###
+connection = psycopg2.connect(user = "jrngriijazlzxr",
+                                  password = "655c5947f8ad0f453bfd4b5d4b80d6374a3a97f706c4533a60be75c3de343817",
+                                  host = "ec2-54-247-72-30.eu-west-1.compute.amazonaws.com",
+                                  port = "5432",
+                                  database = "d9vtdql067f73n")
+cur = connection.cursor()
+
 app = Flask(__name__)
 
 @app.route("/login", methods=["POST"])
@@ -10,7 +21,16 @@ def login():
     email = request.json.get('email')
     password= request.json.get('password')
 
-    return jsonify(email = email, password = password)
+    #  select the user with the email inputted
+    cur.execute("SELECT email FROM customer WHERE email = '%s'" %email)
+    result = cur.fetchall()
+
+    #  if the result retruns nothing return invalid response
+    if(not result):
+        return jsonify(valid_credentials=False)
+
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     app.debug = True
