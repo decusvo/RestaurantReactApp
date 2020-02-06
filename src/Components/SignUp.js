dc65bad6c361ebc6fec4af7c7f48c529f9e99ceb
JamieBliss <zfac223@example.com>
2020-02-06 23:27:41 +0000

    added the ability for the user to sign up using the api

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -16,6 +15,180 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Copyright from "./Copyright";
 import Box from "@material-ui/core/Box";
 
+import React from 'react';
+import withStyles from "@material-ui/core/styles/withStyles";
+
+class SignUp extends React.Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        tAndC: false
+      }
+    }
+
+    handleTAndCChange = (event) => {
+      this.setState({tAndC: !this.state.tAndC})
+    }
+
+    handleTextChange = (event) => {
+      let change = {};
+      change[event.target.name] = event.target.value;
+      this.setState(change)
+    }
+
+    checkPasswords() {
+      return this.state.password === this.state.confirmPassword
+    }
+
+    handleSubmit = (event) => {
+      event.preventDefault()
+      console.log(this.state)
+      if(this.checkPasswords() && this.state.tAndC){
+        let {email, firstName, lastName, password} = this.state
+        fetch("//127.0.0.1:5000/signup", {method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({firstname: firstName,
+                              "lastname": lastName,
+                              "email": email,
+                              "password": password})
+        }).then(response => {
+          return response.json()
+        }).then(data => {
+          console.log(data);
+        }).catch(error => console.log(error))
+      } else{
+        // TODO: show error to the user i.e. the passwords are different
+      }
+    }
+
+    render() {
+      const {classes} = this.props;
+      return (
+          <ThemeProvider theme={theme}>
+              <Container component="main" maxWidth="xs">
+                  <CssBaseline />
+                  <div className={classes.paper}>
+                      <Avatar className={classes.avatar}>
+                          <LockOutlinedIcon />
+                      </Avatar>
+                      <Typography component="h1" variant="h5">
+                          Sign up
+                      </Typography>
+                      <form className={classes.form} onSubmit={this.handleSubmit} method = "post">
+                          <Grid container spacing={2}>
+                              <Grid item xs={12} sm={6}>
+                                  <TextField
+                                      autoComplete="fname"
+                                      name="firstName"
+                                      variant="outlined"
+                                      required
+                                      fullWidth
+                                      id="firstName"
+                                      placeholder="First Name"
+                                      autoFocus
+                                      onChange={this.handleTextChange}
+                                  />
+                              </Grid>
+                              <Grid item xs={12} sm={6}>
+                                  <TextField
+                                      variant="outlined"
+                                      required
+                                      fullWidth
+                                      id="lastName"
+                                      placeholder="Last Name"
+                                      name="lastName"
+                                      autoComplete="lname"
+                                      onChange={this.handleTextChange}
+                                  />
+                              </Grid>
+                              <Grid item xs={12}>
+                                  <TextField
+                                      type = 'email'
+                                      variant="outlined"
+                                      required
+                                      fullWidth
+                                      id="email"
+                                      placeholder="Email Address"
+                                      name="email"
+                                      autoComplete="email"
+                                      onChange={this.handleTextChange}
+                                  />
+                              </Grid>
+                              <Grid item xs={12}>
+                                  <TextField
+                                      variant="outlined"
+                                      required
+                                      fullWidth
+                                      name="password"
+                                      placeholder="Password"
+                                      type="password"
+                                      id="password"
+                                      autoComplete="current-password"
+                                      onChange={this.handleTextChange}
+                                  />
+                              </Grid>
+                              <Grid item xs={12}>
+                                  <TextField
+                                      variant="outlined"
+                                      required
+                                      fullWidth
+                                      name="confirmPassword"
+                                      placeholder="Confirm Password"
+                                      type="password"
+                                      id="passwordConfirmation"
+                                      onChange={this.handleTextChange}
+                                  />
+                              </Grid>
+                              <Grid item xs={12}>
+                                  <FormControlLabel
+                                      control={<Checkbox value={this.state.tAndC}
+                                            color="primary"
+                                            onChange={this.handleTAndCChange} />}
+                                      label="I have read Terms & Conditions"
+                                  />
+                              </Grid>
+                          </Grid>
+                          <Button
+                              type="submit"
+                              fullWidth
+                              variant="contained"
+                              color="primary"
+                              className={classes.submit}
+                          >
+                              Sign Up
+                          </Button>
+                          <Grid container direction={'row'}>
+                              <Grid container justify="flex-end" >
+                                  <Grid item >
+                                      <Link href="#" variant="body1">
+                                          Read Terms & conditions
+                                      </Link>
+                                  </Grid>
+                              </Grid>
+                              <Grid container justify="flex-end">
+                                  <Grid item >
+                                      <Link href="#" variant="body1">
+                                          Already have an account? Sign in
+                                      </Link>
+                                  </Grid>
+                              </Grid>
+                          </Grid>
+                      </form>
+                  </div>
+                  <Box mt={5}>
+                      <Copyright />
+                  </Box>
+              </Container>
+          </ThemeProvider>
+      );
+    }
+}
 
 const useStyles = makeStyles(theme => ({
     paper: {
@@ -44,118 +217,4 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-
-export default function SignUpForm() {
-    const classes = useStyles();
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <form className={classes.form} noValidate>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="fname"
-                                    name="firstName"
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="firstName"
-                                    placeholder="First Name"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="lastName"
-                                    placeholder="Last Name"
-                                    name="lastName"
-                                    autoComplete="lname"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    placeholder="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    placeholder="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    variant="outlined"
-                                    required
-                                    fullWidth
-                                    name="passwordConfirmation"
-                                    placeholder="Confirm Password"
-                                    type="password"
-                                    id="passwordConfirmation"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="I have read Terms & Conditions"
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container direction={'row'}>
-                            <Grid container justify="flex-end" >
-                                <Grid item >
-                                    <Link href="#" variant="body1">
-                                        Read Terms & conditions
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                            <Grid container justify="flex-end">
-                                <Grid item >
-                                    <Link href="#" variant="body1">
-                                        Already have an account? Sign in
-                                    </Link>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </div>
-                <Box mt={5}>
-                    <Copyright />
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
-}
+export default withStyles(useStyles)(SignUp);
