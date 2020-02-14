deb8030901b3a5498c611abc6fcc3a134167caf5
Tomas Riabovas <zfac189@live.rhul.ac.uk>
2020-02-14 01:44:30 +0000

    Added a demo order list , a checkout button and some styling.

--- ui/src/Components/Order.js
+++ ui/src/Components/Order.js
@@ -4,6 +4,8 @@ import Typography from "@material-ui/core/Typography";
 import ListItemText from "@material-ui/core/ListItemText";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {useSelector} from "react-redux";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -11,39 +13,88 @@ const useStyles = makeStyles(theme => ({
         maxWidth: 360,
         backgroundColor: theme.palette.background.paper,
         alignItems: "center",
-        justify: "center"
+        justify: "center",
+        marginLeft: theme.spacing(2),
     },
     inline: {
         display: "inline"
-    }
+    },
+    title: {
+        marginTop: theme.spacing(2),
+        variant:"h2",
+        color:"textSecondary"
+    },
+    checkout: {
+        margin: theme.spacing(3, 0, 2),
+        background: 'linear-gradient(144deg, rgba(252,192,26,1) 0%, rgba(135,211,51,1) 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 40,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        minWidth: 200
+    },
 }));
 
 const MapOrderItem = () => {
     const currentItems = useSelector(state => state.currentItems);
     console.log(currentItems);
-    return currentItems.map(function (dish, index) {
-        if (typeof dish === "string") {
 
-            return (<ListItem key={index} >
-                <ListItemText
-                    primary="Brunch this weekend?"
-                    secondary={
-                        <React.Fragment>
-                            <Typography
-                                component="span"
-                                variant="body2"
-                                color="textPrimary"
-                            >
-                                Ali Connors
-                            </Typography>
-                            {" — I'll be in your neighborhood doing errands this…"}
-                        </React.Fragment>
-                    }
-                />
-            </ListItem>)
-        } else {
-            return (<div key={index}> </div>);
-        }
+    const array = [{
+        name:'Item 1',
+        q:'5'
+    },
+        {
+            name:'Item 2',
+            q:'4'
+        },
+        {
+            name:'Item 3',
+            q:'2'
+        },
+        {
+            name:'Item 4',
+            q:'4'
+        },
+        {
+            name:'Item 5',
+            q:'2'
+        },
+
+    ]
+
+    return array.map(function (dish, index) {
+        const itemName = dish.name;
+        const itemQuantity = dish.q;
+        return (<ListItem key={index} >
+            <ListItemText
+                primary={
+                    <React.Fragment>
+                        <Typography
+                            component="span"
+                            variant="body1"
+                            color="textPrimary"
+                        >
+                            itemName
+                        </Typography>
+
+                    </React.Fragment>
+                }
+                secondary={
+                    <React.Fragment>
+                        <Typography
+                            component="span"
+                            variant="body2"
+                            color="textSecondary"
+                        >
+                            Q : {itemQuantity}
+                        </Typography>
+
+                    </React.Fragment>
+                }
+            />
+        </ListItem>)
     });
 };
 
@@ -51,10 +102,29 @@ const Order = () => {
     const classes = useStyles();
     const currentItems = useSelector(state => state.currentItems);
     return (
-        <List className={classes.root}>
-            {console.log(currentItems)}
-            <MapOrderItem />
-        </List>
+        <React.Fragment >
+            <Typography variant="h3" className={classes.title}>
+                Your order list
+            </Typography>
+
+            <List className={classes.root}>
+                {console.log(currentItems)}
+                <MapOrderItem />
+            </List>
+
+            <Grid container>
+                <Grid item xs={12}  >
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.checkout}
+                    >
+                        Checkout
+                    </Button>
+                </Grid>
+            </Grid>
+        </React.Fragment>
     )
 };
 
