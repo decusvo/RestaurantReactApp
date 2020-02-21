593ad1472a551d091c3a470f2d3e2eb295e3529e
ramesh267 <59914656+ramesh267@users.noreply.github.com>
2020-02-21 01:55:08 +0000

    Added basic availability state change function
    Validation to check if a valid menu item has been passed still needs to be added, as well as the ability to change a state back to true.

--- api/endpoints/menu/menu.py
+++ api/endpoints/menu/menu.py
@@ -4,28 +4,21 @@ import json
 import psycopg2
 from common import connector
 
-connection = connector.get_connection()
-cur = connection.cursor()
 
 bp = Blueprint("menu blueprint", __name__)
 
-@bp.route("/test")
-def test():
-	return "testing new test"
 
-@bp.route("/menu")
+@bp.route("/menu", methods=["POST"])
 def menu():
+	print(request.json)
 	if len(request.args) == 0:	# if there are no arguments select everything
 		#  gets the whole menu from the database and gets the menu item type i.e. side, main ect
 		#  this sql query returns the result as an already formatted json
-		cur.execute("SELECT TO_JSON(t) " +
-			"FROM (SELECT menu.id, name, description, vegan, " +
+		result = connector.json_select("SELECT menu.id, name, description, vegan, " +
 			"gluten_free, vegetarian, calories, price, available, type " +
 				"FROM menu, item_type " +
-				"WHERE item_type.id = menu.food_type)" +
-			"t;")
+				"WHERE item_type.id = menu.food_type")
 		# gets the result from the database
-		result = cur.fetchall()
 		return jsonify(data={"items" : result})
 
 	else:  # not implemented yet
@@ -33,3 +26,12 @@ def menu():
 		# for example get only sides ect by adding '?item_type=side'
 		return jsonify(Error="arguments not implemented yet")
 
+@bp.route("/menu/<string:menu.id>", methods=["POST"])
+#default state of available is TRUE, therefore will only change state to FALSE for time being
+#Does not take in account that an item may already be set to unavailable
+def availabilityChange(menu_Id):
+	#generate query string to update the menu item
+	 query = "UPDATE menu SET available = FALSE WHERE menu.id = (%s)"
+	 result = connector.execute_query(query, (menu_Id,))
+	 return jsonify(data={"menu item of id " : menu.id + "made unavailable"})
+	 #add validation to check if valid menu id has been passed
