30c2189f70e0dd3b183a469191ecf277b49cafb9
vinaykakkar0 <vinay.kakkar01@gmail.com>
2020-02-25 19:56:09 +0000

    Changed method of page by added cards and a button relevent to the trello

--- Kitchen/src/App.js
+++ Kitchen/src/App.js
@@ -1,59 +1,136 @@
-import React from 'react';
-import {CssBaseline, Typography, withStyles} from '@material-ui/core';
+import React from "react";
+import PropTypes from "prop-types";
+import SwipeableViews from "react-swipeable-views";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import Button from '@material-ui/core/Button';
 
 
-//basic styles
-const useStyles = theme => ({
-    root: {
-        flexGrow: 1,
-    },
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
 
-    card: {
-        padding: theme.spacing(6),
-        marginTop: theme.spacing(6),
-        display: 'flex',
-        textAlign: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-        fontSize: 20
-    },
-
-    typography: {
-        marginTop: 10, fontSize: 25
-    }
-});
-
-class Kitchen extends React.Component {
-
-    render() {
-        return (
-            <React.Fragment>
-                <CssBaseline />
-
-                <Grid container spacing={3}>
-                    {/*Grid for the to be confirmed, order objects will later be loaded in dynamically*/}
-                    <Grid item xs>
-                        <Typography color={"textPrimary"} gutterBottom>
-                            In Progress
-                        </Typography>
-                    </Grid>
-
-                    <Grid item xs>
-                        <Typography color={"textPrimary"} gutterBottom>
-                            Currently Cooking
-                        </Typography>
-                    </Grid>
-                </Grid>
-
-                    <Box mt={5}>
-                    </Box>
-            </React.Fragment>
-
-        )
-    }
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`action-tabpanel-${index}`}
+            aria-labelledby={`action-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
 }
 
-export default withStyles(useStyles)(Kitchen);
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired
+};
+
+function a11yProps(index) {
+    return {
+        id: `action-tab-${index}`,
+        "aria-controls": `action-tabpanel-${index}`
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: theme.palette.background.paper,
+        width: "value",
+        position: "relative",
+        minHeight: 200
+    }
+}));
+
+export default function Kitchen() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = index => { // handles change of index in the <Tabs> component.
+        setValue(index);
+    };
+
+
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="dynamic" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="contained"
+                >
+                    <Tab label="To be done" {...a11yProps(0)} />
+                    <Tab label="currently cooking" {...a11yProps(1)} />
+                </Tabs>
+            </AppBar>
+
+            <SwipeableViews
+                axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+
+                <TabPanel value={value} index={0} dir={theme.direction}>
+
+                    <Grid container xs spacing={3}>
+                        <Grid item xs>
+                    <Card className={classes.card}>
+			tacos
+                    </Card>
+                        </Grid>
+                    </Grid>
+                    <Grid container xs spacing={3}>
+                        <Grid item xs>
+                    <Card className={classes.card}>
+			chips
+                    </Card>
+                        </Grid>
+                    </Grid>
+
+
+		    <Button variant="contained" color="default" CookIt>
+      	            	Cook It!
+                    </Button>
+
+                </TabPanel>
+
+
+                <TabPanel value={value} index={1} dir={theme.direction}>
+
+
+                    <Grid container xs spacing={3}>
+                        <Grid item xs>
+                    <Card className={classes.card}>
+			tacos
+                    </Card>
+                        </Grid>
+                    </Grid>
+
+
+		    <Button variant="contained" color="default" Cooked>
+      	            	Cooked!
+                    </Button>
+
+
+                </TabPanel>
+            </SwipeableViews>
+        </div>
+    );
+}
