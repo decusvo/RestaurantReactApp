8af6392f42d41b19158ba51179639251d684a864
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2020-02-26 21:21:25 +0000

    Added an early version of order_event endpoint.
    - The order events are added to the order_event table.
    
    - Validation is partly working but need to correctly handle cases when pgpsql creates an error.

--- api/tests/order_events_test.py
+++ api/tests/order_events_test.py
@@ -1,6 +1,17 @@
+import requests, json, sys
+try:
+	from . import tester
+except:
+	import tester
 
-def test_order_event_no_order_id():
-	req = session.post(api_url + "order_event", json={"order_event" : "request"})
+session = requests.Session()
+
+verbose = False
+
+api_url = "http://localhost:5000/"
+
+def test_order_event_request():
+	req = session.post(api_url + "order_event", json={"order_id" : 1, "order_event" : "request"})
 	if verbose:
 		print(req.text)
 	return req.status_code
@@ -11,3 +22,12 @@ def test_order_event_no_order_event():
 		print(req.text)
 	return req.status_code
 
+tests = [
+	test_order_event_request,
+	test_order_event_request,
+]
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1 and sys.argv[1] == "v":
+		verbose = True
+	tester.run_tests(tests)
