cf7d01ae5b0d2dc21638ec33389f6de01d3e5e30
Tanveer316 <59915809+Tanveer316@users.noreply.github.com>
2020-02-27 23:11:49 +0000

    AppBar

--- Kitchen/src/App.js
+++ Kitchen/src/App.js
@@ -5,6 +5,13 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import OrderItem from "./OrderItem";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 
 //basic styles
 const useStyles = theme => ({
@@ -68,6 +75,20 @@ class WaiterDashboard extends React.Component {
         return (
             <React.Fragment>
                 <CssBaseline />
+
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" className={classes.title}>
+                            OAXHACA
+                        </Typography>
+                        <Button color="inherit">
+                            Past Orders
+                        </Button>
+                    </Toolbar>
+                </AppBar>
 
                 <Grid container spacing={3}>
                     {/*Grid for the to be confirmed, order objects will later be loaded in dynamically*/}
