797d77df3ce8c188f26011f1e4d588c7d65e4f30
Jamie Bliss <zfac223@live.rhul.ac.uk>
2020-03-05 11:36:43 +0000

    added new view to get name and quantity of an order

--- api/sql/view_schema.sql
+++ api/sql/view_schema.sql
@@ -1,5 +1,6 @@
 DROP VIEW IF EXISTS ordered_items_and_price;
 DROP VIEW IF EXISTS total_order_price;
+DROP VIEW IF EXISTS ordered_item_and_quantity;
 
 CREATE VIEW ordered_items_and_price AS
 	SELECT ordered_item_id, order_id, menu_item_id, price
@@ -10,3 +11,9 @@ CREATE VIEW total_order_price AS
 	SELECT order_id, sum(price) AS price
 	FROM ordered_items_and_price
 	GROUP BY order_id;
+
+CREATE VIEW ordered_item_and_quantity AS
+  SELECT order_id, name, count(name) AS quantity
+  FROM ordered_items, menu
+  WHERE menu_item_id = id
+  GROUP BY order_id, name;
