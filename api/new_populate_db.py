742bb7a135132bc584615b4fb2587b6d8bb69858
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2020-03-09 14:33:40 +0000

    Added stubs for new populate_db script

---
+++ api/new_populate_db.py
@@ -0,0 +1,19 @@
+import populate_db
+import requests
+
+
+def populate_db():
+
+	create_data()
+	
+	populate_db.populate()
+
+def create_data():
+	
+	# TODO STUB
+
+	# add orders via api
+
+	# add notifications via api
+
+
