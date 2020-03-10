79ee44dae7d3eddbfca4d7fd835c460596606264
Tanveer316 <59915809+Tanveer316@users.noreply.github.com>
2020-03-10 21:04:40 +0000

    Update OrderTracking.js

--- ui/src/Components/OrderTracking.js
+++ ui/src/Components/OrderTracking.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import PropTypes from "prop-types";
 import SwipeableViews from "react-swipeable-views";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
@@ -86,4 +86,56 @@ export default function WaiterDashboardV2() {
             </AppBar>
         </div>
     );
+}
+*/
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function LinearBuffer() {
+  const classes = useStyles();
+  const [completed, setCompleted] = React.useState(0);
+  const [buffer, setBuffer] = React.useState(10);
+
+  const progress = React.useRef(() => {});
+  React.useEffect(() => {
+    progress.current = () => {
+      if (completed > 100) {
+        setCompleted(0);
+        setBuffer(10);
+      } else {
+        const diff = Math.random() * 5;
+        const diff2 = Math.random() * 5;
+        setCompleted(completed + diff);
+        setBuffer(completed + diff + diff2);
+      }
+    };
+  });
+
+  React.useEffect(() => {
+    function tick() {
+      progress.current();
+    }
+    const timer = setInterval(tick, 500);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={classes.root}>
+      <LinearProgress variant="buffer" value={completed} valueBuffer={buffer} />
+      <LinearProgress variant="buffer" value={completed} valueBuffer={buffer} color="secondary" />
+    </div>
+  );
 }
