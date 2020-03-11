5fca758b09cbb76c8d1c30042ecb187611bd2e87
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2020-03-11 21:43:19 +0000

    Converted unnecessary serial to integers.

--- api/new_populate_db.py
+++ api/new_populate_db.py
@@ -3,15 +3,16 @@ import requests
 
 
 def populate_db():
-
-	create_data()
 	
 	populate_db.populate()
 
+	create_data()
+
 def create_data():
 	
+	url = "http://localhost:5000/create_order"
 	# TODO STUB
-
+	
 	# add orders via api
 
 	# add notifications via api
