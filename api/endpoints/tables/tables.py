e276b1e4809d390c6ca811d42b80223f503984e1
Jamie Bliss <zfac223@live.rhul.ac.uk>
2020-03-12 18:19:30 +0000

    tweaked the return of get_tables just one array of table numbers

--- api/endpoints/tables/tables.py
+++ api/endpoints/tables/tables.py
@@ -9,5 +9,8 @@ bp = Blueprint("tables blueprint", __name__)
 @bp.route("/get_tables", methods=["POST"])
 def get_tables():
     query = "select table_number as table_numbers from table_details"
-    result = connector.json_select(query)
-    return jsonify(data={"tables":result})
+    result = connector.execute_query(query)
+    output = []
+    for ele in result:
+        output.append(ele[0])
+    return jsonify(data={"tables":output})
