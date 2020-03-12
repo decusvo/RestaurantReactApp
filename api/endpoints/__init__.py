34eec702fa477c4b39c4e5bb06b81f1cc98b998e
Jamie Bliss <zfac223@live.rhul.ac.uk>
2020-03-12 19:43:45 +0000

    added new payment endpoint capabilities

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -5,6 +5,7 @@ from . import orders
 from . import sessions
 from . import notifications
 from . import tables
+from . import payments
 
 
 menu_bp = menu.bp
@@ -14,3 +15,4 @@ orders_bp = orders.bp
 sessions_bp = sessions.bp
 notifications_bp = notifications.bp
 tables_bp = tables.bp
+payments_bp = payments.bp
