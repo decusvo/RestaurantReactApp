9522ab3ce25fb5aa1a5e5bd39d7cb6aba28e4cb2
JamieBliss <zfac223@example.com>
2020-03-19 22:32:34 +0000

    added new endpoint to get the waiter at each table

--- api/endpoints/tables/tables.py
+++ api/endpoints/tables/tables.py
@@ -8,9 +8,18 @@ bp = Blueprint("tables blueprint", __name__)
 
 @bp.route("/get_tables", methods=["POST"])
 def get_tables():
-    query = "select table_number as table_numbers from table_details"
+    query = "SELECT table_number AS table_numbers FROM table_details"
     result = connector.execute_query(query)
     output = []
     for ele in result:
         output.append(ele[0])
     return jsonify(data={"tables":output})
+
+@bp.route("/get_tables_and_waiters", methods=["POST"])
+def get_tables_and_waiters():
+    query = "SELECT table_number, email, firstname, lastname "\
+            "FROM table_details, waiter "\
+            "WHERE waiter.email = waiter_id "
+    result = connector.json_select(query)
+
+    return jsonify(data={"tables":result})
