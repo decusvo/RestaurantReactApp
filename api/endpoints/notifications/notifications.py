561473833b1018f04992c95de75f62c9937edb2b
Pickersgill <Jack.Pellen-Pickersgill.2018@live.rhul.ac.uk>
2020-03-19 16:02:28 +0000

    Removed customer notifications endpoints.

--- api/endpoints/notifications/notifications.py
+++ api/endpoints/notifications/notifications.py
@@ -28,20 +28,6 @@ def add_waiter_notification():
 	return jsonify(data={"success" : True})
 
 
-@bp.route("/add_customer_notification", methods=["POST"])
-def add_customer_notification():
-	error = vn.validate_notification(request)
-	if error:
-		return(error)
-	
-	message = request.json.get("message")
-	waiter_id = request.json.get("waiter_id")
-	customer_id = request.json.get("customer_id")
-
-	query = "INSERT INTO customer_notifications(waiter_id, customer_id, message) VALUES(%s, %s, %s)"
-	connector.execute_insert_query(query, (waiter_id, customer_id, message))
-
-	return jsonify(data={"success" : True})
 
 @bp.route("/get_waiter_notifications", methods=["POST"])
 def get_waiter_notifications():
@@ -57,32 +43,6 @@ def get_waiter_notifications():
 	return jsonify(data={"notifications" : notifications, "success" : True})
 	
 		
-@bp.route("/get_customer_notifications", methods=["POST"])
-def get_customer_notifications():
-	error = vn.validate_get_customer_notifications(request)
-	if error:
-		return(error)
-
-	customer = request.json.get("customer")
-
-	query = "SELECT * FROM customer_notifications WHERE customer_id=%s"
-	notifications = connector.execute_query(query, (customer,))
-	
-	return jsonify(data={"notifications" : notifications, "success" : True})
-
-
-@bp.route("/clear_customer_notifications", methods=["POST"])
-def clear_customer_notifications():
-	error = vn.validate_clear_customer_notifications(request)
-	if error:
-		return(error)
-
-	customer = request.json.get("customer")
-
-	query = "DELETE FROM customer_notifications WHERE customer_id=%s"
-	connector.execute_insert_query(query, (customer,))
-
-	return jsonify(data={"success" : True})
 
 
 @bp.route("/clear_waiter_notifications", methods=["POST"])
--- api/endpoints/notifications/validate_notifications.py
+++ api/endpoints/notifications/validate_notifications.py
@@ -26,6 +26,7 @@ def validate_notification(request):
 
 	return None
 
+
 def validate_get_waiter_notifications(request):
 	if "waiter_id" not in request.json:
 		error_msg = "Expected 'waiter_id' argument, none was given"
@@ -36,35 +37,6 @@ def validate_get_waiter_notifications(request):
 	r = connector.execute_query("SELECT * FROM waiter WHERE waiter_id=%s", (waiter_id,))
 	if r is None:
 		error_msg = "Given waiter id does not appear in waiter table"
-		return jsonify(error={"success" : False, "message" : error_msg})
-	
-	return None
-		
-	
-def validate_get_customer_notifications(request):
-	if "customer" not in request.json:
-		error_msg = "Expected 'customer' argument, none was given"
-		return jsonify(error={"success" : False, "message" : error_msg})
-	
-	customer = request.json.get("customer")
-	
-	r = connector.execute_query("SELECT * FROM customer WHERE email=%s", (customer,))
-	if r is None:
-		error_msg = "Given customer email does not appear in customer table"
-		return jsonify(error={"success" : False, "message" : error_msg})
-	
-	return None
-
-def validate_clear_customer_notifications(request):
-	if "customer" not in request.json:
-		error_msg = "Expected 'customer' argument, none was given"
-		return jsonify(error={"success" : False, "message" : error_msg})
-	
-	customer = request.json.get("customer")
-	
-	r = connector.execute_query("SELECT * FROM customer WHERE email=%s", (customer,))
-	if r is None:
-		error_msg = "Given customer email does not appear in customer table"
 		return jsonify(error={"success" : False, "message" : error_msg})
 	
 	return None
