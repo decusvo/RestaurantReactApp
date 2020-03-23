9e362459de93545db6cc8f70b4370aca12ad1799
Tomas Riabovas <zfac189@live.rhul.ac.uk>
2020-03-23 23:23:02 +0000

    Added comments to functions in PaymentForm.js

--- ui/src/Components/Payment/PaymentForm.js
+++ ui/src/Components/Payment/PaymentForm.js
@@ -61,6 +61,9 @@ export default function PaymentForm() {
   const [message, setMessage] = React.useState("");
   const [open, setOpenAlert] = React.useState(false);
 
+  // submitPayment is a function which sends customer details to be verified by the API which were collected from the respective textfields.
+  // The API response is saved locally, as well as the customers name.
+
   const submitPayment = () => {
     fetch("//127.0.0.1:5000/verify_payment", {
       method: "POST",
@@ -88,13 +91,15 @@ export default function PaymentForm() {
       .catch(error => console.log(error));
   };
 
+  // updateOrder is a function which sends a call to the API endpoint. Upon successful verification, the function tells the API that the particular order ID has been paid.
+
   const updateOrder = () => {
     fetch("//127.0.0.1:5000/order_event", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        "order_id": localStorage.getItem("ProcessedOrderID"),
-        "order_event": "pay"
+        order_id: localStorage.getItem("ProcessedOrderID"),
+        order_event: "pay"
       })
     })
       .then(response => {
@@ -106,12 +111,16 @@ export default function PaymentForm() {
       });
   };
 
+  // invalidPayment is a function which gives the user a notification if the API cannot successfully verify details.
+
   const invalidPayment = () => {
     setMessage(
       "There has been a problem validating your payment. Please check if information entered is correct."
     );
     setOpenAlert(true);
   };
+
+  // handleClose is responsible for closing the alert that notifies the user of an issue with verifying the payment.
 
   const handleClose = (event, reason) => {
     // Handles the closing of a notification.
@@ -121,33 +130,53 @@ export default function PaymentForm() {
     setOpenAlert(false);
   };
 
+  // handleSortCode function takes the value entered in the sort code text field and updates the value of the CardSortCode( state accordingly.
+
   const handleSortCode = event => {
     setCardSortCode(event.target.value);
   };
+
+  // handleName function takes the value entered in the cardholder name text field and updates the values of the CardName state.
+
   const handleName = event => {
     setCardName(event.target.value);
   };
+
+  // handleNumber function takes the value entered in the cardholder name text field and updates the values of the CardNumber state.
+
   const handleNumber = event => {
     setCardNumber(event.target.value);
   };
+
+  // handleExpiry function takes the value entered in the cardholder name text field and updates the values of the CardExpiry state.
+
   const handleExpiry = event => {
     setCardExpiry(event.target.value);
   };
+
+  // handleCVV function takes the value entered in the cardholder name text field and updates the values of the CardCVV state.
 
   const handleCVV = event => {
     setCardCVV(event.target.value);
   };
 
+  // handleBack is responsible for changing the state of RedirectToSummary. Once set to true, a ternary expression redirects the user to OrderSummary page.
+
   const handleBack = () => {
     setRedirectToSummary(true);
   };
 
+  // uponConfirmation function is responsible for calling the function to update the order and then redirect the customer to the post payment page.
+
   const uponConfirmation = () => {
-    console.log("Order being updated.");
     updateOrder();
 
     return <Redirect to="/PostPaymentPage" />;
   };
+
+  // The return clause renders a React element with a payment form to fill out for the customer.
+  // Upon paying, a response is given. If the response is successful the customer is redirected.
+  // If the API response is not a success, then the customer is notified via an on-screen alert.
 
   return (
     <React.Fragment>
