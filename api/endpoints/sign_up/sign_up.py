ecb2b5435db98654bd56133f6445ce79f463601b
Jamie Bliss <zfac223@live.rhul.ac.uk>
2020-03-26 14:08:57 +0000

    added validition option to signup endpoint

--- api/endpoints/sign_up/sign_up.py
+++ api/endpoints/sign_up/sign_up.py
@@ -1,12 +1,16 @@
 from flask import Flask, request, jsonify, Blueprint
 import psycopg2
 from common import connector
-
+from . import validate_sign_up
 
 bp = Blueprint("signup blueprint", __name__)
 
 @bp.route("/signup", methods=["POST"])
 def sign_up():
+    error = validate_sign_up.validate_user(request)
+    if error:
+        return error
+
     #  Get the details of the user from a post request as a json
     email = request.json.get('email')
     password = request.json.get('password')
---
+++ api/endpoints/sign_up/validate_sign_up.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, jsonify, Blueprint
+import json
+import psycopg2
+from common import connector
+
+def validate_user(request):
+    return None
