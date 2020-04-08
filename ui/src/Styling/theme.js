14f04f2fea8d2e7af4598404579d831f9a0c3c92
Tomas Riabovas <zfac189@live.rhul.ac.uk>
2020-04-08 16:39:14 +0100

    Reverted back to previous API.

--- ui/src/Styling/theme.js
+++ ui/src/Styling/theme.js
@@ -1,23 +1,32 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 // A custom theme for this app
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#87D333',
-        },
-        secondary: {
-            main: '#68a4a7',
-        },
-        error: {
-            main: red.A400,
-        },
-        background: {
-            default: '#fff'
-        },
+  palette: {
+    primary: {
+      main: "#87D333"
     },
+    secondary: {
+      main: "#fbc531"
+    },
+    error: {
+      main: red.A400
+    },
+    background: {
+      default: "#fff"
+    }
+  },
 
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          backgroundColor: "#f5f6fa"
+        }
+      }
+    }
+  }
 });
 
 export default theme;
