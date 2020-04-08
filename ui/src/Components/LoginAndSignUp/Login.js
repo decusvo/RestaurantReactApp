56d6899fd7a43f1d97b9f4671ffbc64c482734f7
Tomas Riabovas <zfac189@live.rhul.ac.uk>
2020-04-08 14:26:58 +0100

    Removed unnecessary styling.

--- ui/src/Components/LoginAndSignUp/Login.js
+++ ui/src/Components/LoginAndSignUp/Login.js
@@ -297,17 +297,6 @@ const useStyles = theme => ({
     height: 40,
     padding: "0 30px",
     boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
-  },
-  logout: {
-    margin: theme.spacing(0, 0, 1),
-    background:
-      "linear-gradient(144deg, rgba(252,192,26,1) 0%, rgba(135,211,51,1) 90%)",
-    borderRadius: 3,
-    border: 0,
-    color: "white",
-    height: 40,
-    padding: "0 30px",
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
   }
 });
 
