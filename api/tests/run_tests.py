7e6c8fd69840b1004c91da7df5737db4afcaec44
JamieBliss <zfac223@live.rhul.ac.uk>
2020-04-09 16:52:27 +0100

    refactored code

--- api/tests/run_tests.py
+++ api/tests/run_tests.py
@@ -10,14 +10,20 @@ if len(sys.argv) > 1 and sys.argv[1] == "v":
 	order_test.verbose = True
 	notification_test.verbose = True
 
+all_tests = [
+			login_test,
+			session_test,
+			menu_test,
+			order_test,
+			notification_test
+			]
+
 total = 0
 passed = 0
 failed = []
-total, passed, failed = tester.run_tests(login_test.tests, total, passed, failed)
-total, passed, failed = tester.run_tests(session_test.tests, total, passed, failed)
-total, passed, failed = tester.run_tests(menu_test.tests, total, passed, failed)
-total, passed, failed = tester.run_tests(order_test.tests, total, passed, failed)
-total, passed, failed = tester.run_tests(notification_test.tests, total, passed, failed)
+
+for test in all_tests:
+	total, passed, failed = tester.run_tests(test.tests, total, passed, failed)
 
 tester.print_results(total, passed, failed)
 
