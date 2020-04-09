c080af2bfd6bfafe0a5c741d6c68972a93c7939e
JamieBliss <zfac223@live.rhul.ac.uk>
2020-04-09 14:52:29 +0100

    implementing feature to see amount of failed and passed tests

--- api/tests/run_tests.py
+++ api/tests/run_tests.py
@@ -2,6 +2,7 @@ from . import login_test, session_test, menu_test, order_test, notification_test
 from . import tester
 import sys
 
+
 if len(sys.argv) > 1 and sys.argv[1] == "v":
 	login_test.verbose = True
 	session_test.verbose = True
@@ -9,9 +10,14 @@ if len(sys.argv) > 1 and sys.argv[1] == "v":
 	order_test.verbose = True
 	notification_test.verbose = True
 
-tester.run_tests(login_test.tests)
-tester.run_tests(session_test.tests)
-tester.run_tests(menu_test.tests)
-tester.run_tests(order_test.tests)
-tester.run_tests(notification_test.tests)
+total = 0
+passed = 0
+failed = []
+total, passed, failed = tester.run_tests(login_test.tests, total, passed, failed)
+total, passed, failed = tester.run_tests(session_test.tests, total, passed, failed)
+total, passed, failed = tester.run_tests(menu_test.tests, total, passed, failed)
+total, passed, failed = tester.run_tests(order_test.tests, total, passed, failed)
+total, passed, failed = tester.run_tests(notification_test.tests, total, passed, failed)
+
+tester.print_results(total, passed, failed)
 
