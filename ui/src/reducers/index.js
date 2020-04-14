8efcfea417d8a375d6a3df2612cfba7e12d5e972
dnzggg <45202160+dnzggg@users.noreply.github.com>
2020-04-14 16:47:39 +0100

    Added jsdoc for Reducers

--- ui/src/reducers/index.js
+++ ui/src/reducers/index.js
@@ -2,9 +2,13 @@ import currentUser from "./currentUser";
 import currentItems from "./currentItems";
 import {combineReducers} from "redux";
 
+/**
+ * @module Reducers
+ */
 
-
-
+/**
+ * Combines all the reducers given so it can be used in index.js as a global state
+ */
 const rootReducer = combineReducers({
     currentItems, currentUser
 });
